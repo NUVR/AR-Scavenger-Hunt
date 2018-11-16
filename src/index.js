aafe7f0c8fc0d3c05827ed97296b2c0bb7d31638
Douglas Schonholtz <schonholtz.d@husky.neu.edu>
2018-11-15 22:16:29 -0500

    pushed code

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ import {
   Scene,
   TextureLoader,
   WebGLRenderer,
+  AnimationMixer,
+  LoopRepeat,
 } from 'three';
+
+import GLTFLoader from 'three-gltf-loader';
 
 import './style.scss';
 
@@ -28,14 +32,16 @@ const light = new AmbientLight({ color: 0x1f1f1f });
 const ptLight = new PointLight({ color: 0xffffff });
 ptLight.position.y = 200;
 
+let mixer, ready;
+
 scene.add(light);
 scene.add(ptLight);
 
-const texture = new TextureLoader().load('assets/crate.gif');
-const material = new MeshPhongMaterial({ map: texture, side: DoubleSide });
-const mesh = new Mesh(new BoxBufferGeometry(1, 1, 1), material);
-mesh.position.y = 0.5;
-scene.add(mesh);
+// const texture = new TextureLoader().load('assets/crate.gif');
+// const material = new MeshPhongMaterial({ map: texture, side: DoubleSide });
+// const mesh = new Mesh(new BoxBufferGeometry(1, 1, 1), material);
+// mesh.position.y = 0.5;
+// scene.add(mesh);
 
 const arToolkitContext = new ArToolkitContext({
   cameraParametersUrl: 'assets/camera_para.dat',
@@ -51,6 +57,56 @@ const markerControls = new ArMarkerControls(arToolkitContext, camera, {
   patternUrl: 'assets/patt.hiro',
   changeMatrixMode: 'cameraTransformMatrix',
 });
+
+initModel();
+
+function initModel() {
+  var loader = new GLTFLoader();
+  loader.load('assets/Models/ugandan_knuckles/scene.gltf', function(gltf) {
+    var model = gltf.scene;
+    model.scale.set(0.005, 0.005, 0.005);
+    // model.traverse( function ( o ) {
+    //     if ( !o.isMesh ) return;
+    //     o.material.emissive = o.material.color.clone().multiplyScalar( 0.3 );
+    // } );
+    mixer = new AnimationMixer(model);
+    var run = gltf.animations.find(function ( clip ) {
+      return clip.name === 'WalkCycle';
+    });
+    var action = mixer.clipAction(run);
+    action.setLoop(LoopRepeat);
+    action.play();
+    scene.add(model);
+    ready = true;
+  });
+}
+
+// Instantiate a loader
+var loader = new GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+  // resource URL
+  'assets/Models/ugandan_knuckles/scene.gltf',
+  // called when the resource is loaded
+  function(gltf) {
+    gltf.scene.position.set(-1, -0.5, -1);
+    gltf.scene.scale.set(0.0005, 0.0005, 0.0005);
+    scene.add(gltf.scene);
+    // let mixer = new AnimationMixer(gltf.scene);
+    // let action = mixer.clipAction(gltf.animations[0]);
+    // action.setLoop(LoopRepeat);
+    // action.play();
+  },
+  // called while loading is progressing
+  function(xhr) {
+    console.log((xhr.loaded / xhr.total * 100 ) + '% loaded');
+  },
+  // called when loading has errors
+  function(error) {
+    console.log('An error happened' + error);
+  }
+);
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);

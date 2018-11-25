3841447256c8554dded73a6f9f7147ae96c35402
Douglas Schonholtz <schonholtz.d@husky.neu.edu>
2018-11-25 15:57:12 -0500

    Adding movement and rotation for giggles

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ const scene = new Scene();
 scene.add(camera);
 scene.visible = false;
 
+let model = null;
+
 const light = new AmbientLight({ color: 0x1f1f1f });
 const ptLight = new PointLight({ color: 0xffffff });
 ptLight.position.y = 200;
@@ -54,7 +56,7 @@ initModel();
 function initModel() {
   var loader = new GLTFLoader();
   loader.load('assets/Models/ugandan_knuckles/scene.gltf', function(gltf) {
-    const model = gltf.scene;
+    model = gltf.scene;
     model.scale.set(0.00125, 0.00125, 0.00125);
     mixer = new AnimationMixer(model);
     const run = gltf.animations[0];
@@ -62,6 +64,8 @@ function initModel() {
     action.setLoop(LoopRepeat);
     action.play();
     scene.add(model);
+    model.position.z -= 0.6;
+    model.position.x -= 0.45;
     ready = true;
   });
 }
@@ -96,6 +100,50 @@ function render() {
   }
 
   if (ready) {
+    updatePosition();
     mixer.update(clock.getDelta());
   }
+}
+
+let xv = 0.02;
+let zv = 0.01;
+
+function updatePosition() {
+  /*
+      If we are within .1 of -.5 for any then start adding
+     */
+  if (model.position.x < -0.4) {
+    xv += 0.001;
+  }
+  if (model.position.z < -0.4) {
+    zv += 0.001;
+  }
+  if (model.position.x > 0.4) {
+    xv -= 0.001;
+  }
+  if (model.position.z > 0.4) {
+    zv -= 0.001;
+  }
+
+  xv += Math.random() * (0.001 - -0.001) + -0.001;
+  zv += Math.random() * (0.001 - -0.001) + -0.001;
+
+  // This is really sad but I don't remember how to do this with trig and this works:
+  let rotation = (Math.abs(xv) / Math.abs(zv)) * 1.57;
+  if (xv > 0 && zv > 0) {
+    // 0 to pi/2
+  } else if (xv < 0 && zv > 0) {
+    // pi/2 -> pi
+    rotation += 1.57;
+  } else if (xv < 0 && zv < 0) {
+    // pi -> 3/4 pi
+    rotation += 3.14;
+  } else {
+    // 3/4 pi to 2 pi
+    rotation += 4.71;
+  }
+  model.rotation.y = rotation;
+
+  model.position.x += xv;
+  model.position.z += zv;
 }

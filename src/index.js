6c7b0091c191a4ba9222a92862e55aaf34d13f17
Douglas Schonholtz <schonholtz.d@husky.neu.edu>
2018-12-18 23:59:41 -0500

    adding a little rotation. Lighting could still be worked on. Map Renders

--- src/index.js
+++ src/index.js
@@ -27,9 +27,9 @@ scene.visible = false;
 
 const light = new AmbientLight(0x404040);
 const ptLight = new PointLight(0xffffff);
-ptLight.position.set(-1, 2, 2);
+ptLight.position.set(-1, 4, 4);
 
-let mixer, ready;
+let mixer, ready, model;
 
 scene.add(light);
 scene.add(ptLight);
@@ -53,19 +53,27 @@ initModel();
 
 function initModel() {
   var loader = new GLTFLoader();
-  loader.load('assets/Models/ugandan_knuckles/scene.gltf', function(gltf) {
+  loader.load('assets/Models/NortheasternMap2/BostonFromAltizure2-0Cut.gltf', function(gltf) {
     model = gltf.scene;
-    model.scale.set(0.00125, 0.00125, 0.00125);
-    mixer = new AnimationMixer(model);
-    const run = gltf.animations[0];
-    const action = mixer.clipAction(run);
-    action.setLoop(LoopRepeat);
-    action.play();
+    model.position.y += 0.5
+    model.rotation.x -= Math.PI / 9; // 20 degree angle?
+    model.scale.set(0.75, 0.75, 0.75);
     scene.add(model);
-    model.position.z -= 0.6;
-    model.position.x -= 0.45;
     ready = true;
   });
+  // loader.load('assets/Models/ugandan_knuckles/scene.gltf', function(gltf) {
+  //   model = gltf.scene;
+  //   model.scale.set(0.00125, 0.00125, 0.00125);
+  //   mixer = new AnimationMixer(model);
+  //   const run = gltf.animations[0];
+  //   const action = mixer.clipAction(run);
+  //   action.setLoop(LoopRepeat);
+  //   action.play();
+  //   scene.add(model);
+  //   model.position.z -= 0.6;
+  //   model.position.x -= 0.45;
+  //   ready = true;
+  // });
 }
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -98,50 +106,50 @@ function render() {
   }
 
   if (ready) {
-    updatePosition();
-    mixer.update(clock.getDelta());
+    // updatePosition();
+    // mixer.update(clock.getDelta());
   }
 }
 
-let xv = 0.02;
-let zv = 0.01;
+// let xv = 0.02;
+// let zv = 0.01;
 
-function updatePosition() {
-  /*
-      If we are within .1 of -.5 for any then start adding
-     */
-  if (model.position.x < -0.4) {
-    xv += 0.001;
-  }
-  if (model.position.z < -0.4) {
-    zv += 0.001;
-  }
-  if (model.position.x > 0.4) {
-    xv -= 0.001;
-  }
-  if (model.position.z > 0.4) {
-    zv -= 0.001;
-  }
+// function updatePosition() {
+//   /*
+//       If we are within .1 of -.5 for any then start adding
+//      */
+//   if (model.position.x < -0.4) {
+//     xv += 0.001;
+//   }
+//   if (model.position.z < -0.4) {
+//     zv += 0.001;
+//   }
+//   if (model.position.x > 0.4) {
+//     xv -= 0.001;
+//   }
+//   if (model.position.z > 0.4) {
+//     zv -= 0.001;
+//   }
 
-  xv += Math.random() * 0.001 - 0.0005;
-  zv += Math.random() * 0.001 - 0.0005;
+//   xv += Math.random() * 0.001 - 0.0005;
+//   zv += Math.random() * 0.001 - 0.0005;
 
-  // This is really sad but I don't remember how to do this with trig and this works:
-  let rotation = (Math.abs(xv) / Math.abs(zv)) * (Math.PI / 2);
-  if (xv > 0 && zv > 0) {
-    // 0 to pi/2
-  } else if (xv < 0 && zv > 0) {
-    // pi/2 -> pi
-    rotation += Math.PI / 2;
-  } else if (xv < 0 && zv < 0) {
-    // pi -> 3/2 pi
-    rotation += Math.PI;
-  } else {
-    // 3/2 pi to 2 pi
-    rotation += Math.PI * 0.75;
-  }
-  model.rotation.y = rotation;
+//   // This is really sad but I don't remember how to do this with trig and this works:
+//   let rotation = (Math.abs(xv) / Math.abs(zv)) * (Math.PI / 2);
+//   if (xv > 0 && zv > 0) {
+//     // 0 to pi/2
+//   } else if (xv < 0 && zv > 0) {
+//     // pi/2 -> pi
+//     rotation += Math.PI / 2;
+//   } else if (xv < 0 && zv < 0) {
+//     // pi -> 3/2 pi
+//     rotation += Math.PI;
+//   } else {
+//     // 3/2 pi to 2 pi
+//     rotation += Math.PI * 0.75;
+//   }
+//   model.rotation.y = rotation;
 
-  model.position.x += xv;
-  model.position.z += zv;
-}
+//   model.position.x += xv;
+//   model.position.z += zv;
+// }

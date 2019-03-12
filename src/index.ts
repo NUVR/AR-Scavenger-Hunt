285bfe0625fa3183aaf38529f00122bfd366fb80
Abbondanzo <peter@abbondanzo.com>
2019-03-11 22:32:52 -0400

    Use correct environment variable

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ new RootScene();
 // Only build firebase app in staging
 if (process.env.NODE_ENV === 'staging') {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_DEPLOY_TOKEN,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: 'ar-scavenger-hunt-51da3.firebaseapp.com',
     databaseURL: 'https://ar-scavenger-hunt-51da3.firebaseio.com',
     projectId: 'ar-scavenger-hunt-51da3',
